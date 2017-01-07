22b0332c1f25ef33bde27d5110d3fe042847068c
iamyeung <junting.yeung@gmail.com>
2017-01-08 02:37:01 +1100

    Question Selection Page changes for access

--- app/src/main/java/com/example/comp211p/newquiz/QuestionSelectionPage.java
+++ app/src/main/java/com/example/comp211p/newquiz/QuestionSelectionPage.java
@@ -1,10 +1,19 @@
 package com.example.comp211p.newquiz;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment;
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment1;
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment2;
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment3;
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment4;
+import com.example.comp211p.newquiz.quizInterfaceMultipleFragment.QuizInterfaceMultipleFragment5;
 
 public class QuestionSelectionPage extends AppCompatActivity {
 
@@ -13,8 +22,6 @@ public class QuestionSelectionPage extends AppCompatActivity {
     private Button button3;
     private Button button4;
     private Button button5;
-    private Button quitButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,7 @@ public class QuestionSelectionPage extends AppCompatActivity {
             public void onClick(View v) {
                 QuizApp logic = (QuizApp) getApplicationContext();
                 logic.setCurrentQuestion(1);
-                Intent in = new Intent(getApplicationContext(), QuizInterface1.class);
+                Intent in = new Intent(getBaseContext(), QuizInterfaceMultipleFragment1.class);
                 startActivity(in);
             }
         });
@@ -38,7 +45,7 @@ public class QuestionSelectionPage extends AppCompatActivity {
             public void onClick(View v) {
                 QuizApp logic = (QuizApp) getApplicationContext();
                 logic.setCurrentQuestion(2);
-                Intent in = new Intent(getApplicationContext(), QuizInterface1.class);
+                Intent in = new Intent(getBaseContext(), QuizInterfaceMultipleFragment2.class);
                 startActivity(in);
             }
         });
@@ -49,7 +56,7 @@ public class QuestionSelectionPage extends AppCompatActivity {
             public void onClick(View v) {
                 QuizApp logic = (QuizApp) getApplicationContext();
                 logic.setCurrentQuestion(3);
-                Intent in = new Intent(getApplicationContext(), QuizInterface1.class);
+                Intent in = new Intent(getBaseContext(), QuizInterfaceMultipleFragment3.class);
                 startActivity(in);
             }
         });
@@ -60,7 +67,7 @@ public class QuestionSelectionPage extends AppCompatActivity {
             public void onClick(View v) {
                 QuizApp logic = (QuizApp) getApplicationContext();
                 logic.setCurrentQuestion(4);
-                Intent in = new Intent(getApplicationContext(), QuizInterface1.class);
+                Intent in = new Intent(getBaseContext(), QuizInterfaceMultipleFragment4.class);
                 startActivity(in);
             }
         });
@@ -71,11 +78,11 @@ public class QuestionSelectionPage extends AppCompatActivity {
             public void onClick(View v) {
                 QuizApp logic = (QuizApp) getApplicationContext();
                 logic.setCurrentQuestion(5);
-                Intent in = new Intent(getApplicationContext(), QuizInterface1.class);
+                Intent in = new Intent(getBaseContext(), QuizInterfaceMultipleFragment5.class);
                 startActivity(in);
             }
         });
-        
+
         // disable buttons if questions have been answered
         QuizApp logic = (QuizApp) getApplicationContext();
         // check which questions the active players has already answered
@@ -104,18 +111,29 @@ public class QuestionSelectionPage extends AppCompatActivity {
         // switch from player 1 to player 2
         logic.switchPlayer();
         // TODO: REPEAT ALL QUESTIONS FOR PLAYER 2 (go to question selection page)
+        Intent in = new Intent(getApplicationContext(), QuestionSelectionPage.class);
+        startActivity(in);
     }
 
     /**
      * Disables one of the five question buttons (should be used when question is already answered)
-     * @param num Number of the question button to be disabled: 1-5
+     * param num Number of the question button to be disabled: 1-5
      */
-    public void disableButton(int num)
+
+    public void disableButton(Button buttonNumber)
     {
-        if (num>=0 && num <= 5) {
-            //TODO
-            // will be accessed when a question has been answered
-        }
+        buttonNumber.setEnabled(false);
+        //buttonNumber is a selection from Q1, Q2, Q3, Q4, Q5
+        // will be accessed when a question has been answered
+    }
+
+    public void disableAllButtons()
+    {
+        disableButton(button1);
+        disableButton(button2);
+        disableButton(button3);
+        disableButton(button4);
+        disableButton(button5);
     }
 
 }
