a9946eaa1334f0898964a840df6b8062822157f1
annetteloidolt <annette.loidolt@gmx.at>
2017-01-08 01:16:14 +0100

    Added user input validation for name input. Removed Quit Button from QuestionSelectionPage.

--- app/src/main/java/com/example/comp211p/newquiz/InputName.java
+++ app/src/main/java/com/example/comp211p/newquiz/InputName.java
@@ -8,6 +8,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class InputName extends AppCompatActivity {
 
     @Override
@@ -20,20 +22,42 @@ public class InputName extends AppCompatActivity {
 
         toQuestionPageButton.setOnClickListener(new OnClickListener() {
             public void onClick(View view) {
-                String playerName = ((EditText)findViewById(R.id.singlePlayerName)).getText().toString();
-                QuizApp logic = (QuizApp) getApplicationContext();
-                // adds player to the game: if player 1 already exists and game is in multi-player
-                // mode, this adds player 2
-                logic.addPlayer(playerName);
+                EditText inputField = (EditText)findViewById(R.id.singlePlayerName);
+                String playerName = inputField.getText().toString();
+                // some error handling on user input
+                boolean inputError = false;
+                // cant be empty
+                if (playerName.length() == 0){
+                    inputError = true;
+                    inputField.setError("A name is required!");
+                }
+                // max length
+                if (playerName.length() > 8){
+                    inputError = true;
+                    inputField.setError("Name cannot be longer than 8 characters!");
+                }
+                // use regex to validate input
+                Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+                // no special character used (other than alpha numeric)
+                if (p.matcher(playerName).find()) {
+                    inputError = true;
+                    inputField.setError("Only alphanumeric characters allowed!");
+                }
 
-                if (logic.arePlayersReady())
-                {
-                    Intent in = new Intent(getBaseContext(), QuestionSelectionPage.class);
-                    startActivity(in);
-                } else {
-                    // repeats input name for player 2
-                    Intent in = new Intent(getBaseContext(), InputName.class);
-                    startActivity(in);
+                if (!inputError) {
+                    QuizApp logic = (QuizApp) getApplicationContext();
+                    // adds player to the game: if player 1 already exists and game is in multi-player
+                    // mode, this adds player 2
+                    logic.addPlayer(playerName);
+
+                    if (logic.arePlayersReady()) {
+                        Intent in = new Intent(getBaseContext(), QuestionSelectionPage.class);
+                        startActivity(in);
+                    } else {
+                        // repeats input name for player 2
+                        Intent in = new Intent(getBaseContext(), InputName.class);
+                        startActivity(in);
+                    }
                 }
             }
         });
